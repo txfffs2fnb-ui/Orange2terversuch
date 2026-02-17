434d1983c3735b85e2102f705db8f04ee7904317
txfffs2fnb-ui <txfffs2fnb@privaterelay.appleid.com>
2026-02-17 19:03:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
